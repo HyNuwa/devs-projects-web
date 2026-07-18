2d530abaa3065affe496bc3fff24adc24dc76e1e
4lexxe <alexxe@fi.unju.edu.ar>
2026-07-17 23:18:03 -0300

    Frontend: reemplaza emojis por imágenes en ModuleCards

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import { HeroBanner } from '@/components/home/HeroBanner';
+import { ModuleCards } from '@/components/home/ModuleCards';
 
 export default function Home() {
   return (
     <div className="home-page">
       <HeroBanner />
+      <ModuleCards />
       {/* Other sections will go here */}
     </div>
   );
